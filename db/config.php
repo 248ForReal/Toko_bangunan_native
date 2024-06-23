591e6f0429bbcd3172558c8868bcb7dba3890247
248ForReal <masrauf123@gmail.com>
2024-06-23 13:17:32 +0700

    integretaion : config.db

--- db/config.php
+++ db/config.php
@@ -1,12 +1,29 @@
 <?php
 
-DEFINE("HOST", "localhost");
-DEFINE("USER", "root");
-DEFINE("PASS", "");
-DEFINE("DB", "tokobangunan_offline");
-$conn = new mysqli(HOST,USER,PASS,DB);
-if($conn->connect_errno){
-	die("Koneksi Gagal : ". $conn->connect_errno);
+
+DEFINE("HOST_OFFLINE", "localhost");
+DEFINE("USER_OFFLINE", "root");
+DEFINE("PASS_OFFLINE", "");
+DEFINE("DB_OFFLINE", "tokobangunan_offline");
+
+$conn_offline = new mysqli(HOST_OFFLINE, USER_OFFLINE, PASS_OFFLINE, DB_OFFLINE);
+
+if ($conn_offline->connect_errno) {
+    die("Koneksi ke database offline gagal: " . $conn_offline->connect_errno);
 }
+
+DEFINE("HOST_ONLINE", "sql12.freesqldatabase.com");
+DEFINE("USER_ONLINE", "sql12715284");
+DEFINE("PASS_ONLINE", "pxunvyhptv");
+DEFINE("DB_ONLINE", "sql12715284");
+
+$conn_online = new mysqli(HOST_ONLINE, USER_ONLINE, PASS_ONLINE, DB_ONLINE);
+
+if ($conn_online->connect_errno) {
+    die("Koneksi ke database online gagal: " . $conn_online->connect_errno);
+}
+
+// Jika kedua koneksi berhasil
+echo "Koneksi ke kedua database berhasil";
 
 ?>
